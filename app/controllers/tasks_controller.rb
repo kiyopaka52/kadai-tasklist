9d6cc956efa346388a6c2ece5f6b69ead004a925
kenaal <kenaal.52@gmail.com>
2021-06-04 07:10:30 +0000

    login kadai ver.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,13 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    if logged_in?
+       @tasks= current_user.tasks#.order(id: :desc).page(params[:page])
+    #@tasks = Task.all
+    end
   end
   
   def show
@@ -13,11 +18,12 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
+   # @task = Task.new(task_params)
     
     if @task.save
       flash[:success] = 'タスクが作成されました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'タスクが作成されませんでした'
       render :new
@@ -42,6 +48,13 @@ class TasksController < ApplicationController
     
     flash[:success] = 'タスクは削除されました'
     redirect_to tasks_url
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 
   private
